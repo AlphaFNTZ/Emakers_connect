c174afd85e903bf7ca75d444cd2cc050df0cc7c1
AlphaFNTZ <juliano.ramos@estudante.ufla.br>
2024-09-15 23:23:32 -0300

    Començando a criar a tela 5

--- src/Screens/Screen5.jsx
+++ src/Screens/Screen5.jsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import { styled } from "styled-components";
 import BackGround_5 from "../../public/assets/Screen_5.png";
+import Logo_Branco from "../../public/assets/Logo_footer_branco.png";
+import Gengar from "../../public/assets/Sprite/gengar.gif";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Screen5 = styled.section`
 	display: flex;
 	position: relative;
-	z-index: -2;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
@@ -17,10 +22,14 @@ const Screen5 = styled.section`
 
 const FullWidthBox = styled.div`
 	position: absolute;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	width: 100%;
 	height: 486px;
 	bottom: 170px;
 	background-color: white;
+	color: black;
 	@media (max-width: 767px) {
 		bottom: 173px;
 		height: 455px;
@@ -36,17 +45,123 @@ const FooterStyled = styled.footer`
 `;
 
 const FooterPart1 = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
 	height: 128px;
 	width: 100%;
 	background-color: #111111;
 `;
 
+const FooterPart2 = styled.div`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	height: 42px;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Icons = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	gap: 20px;
+	width: 100%;
+
+	.MuiSvgIcon-root {
+		font-size: 45px;
+		cursor: pointer;
+	}
+`;
+
+const Icon_Button = styled.a`
+	color: white;
+`;
+
+const H1_Styled = styled.h1`
+	position: absolute;
+	font-family: "ArcadeClassic", sans-serif;
+	top: 200px;
+	font-size: 70px;
+	font-weight: 200;
+	margin: 0;
+	padding: 0;
+	text-align: center;
+	line-height: 0.7;
+	overflow-wrap: break-word;
+`;
+
+const P_Styled = styled.p`
+	font-family: "ArcadeClassic", sans-serif;
+	font-size: 24px;
+	font-weight: 200;
+	margin: 0;
+	padding: 0;
+	line-height: 0.7;
+`;
+
+const Icon_Footer = styled.div`
+	position: absolute;
+	left: 5px;
+	img {
+		width: 38px;
+	}
+`;
+
+const Caracter_Gengar = styled.div`
+	position: absolute;
+	z-index: 2;
+	top: 300px;
+`;
+
 function Screen_5() {
 	return (
 		<Screen5 id="screen5">
-			<FullWidthBox />
+			<H1_Styled>Contamos com a sua presenca</H1_Styled>
+			{/*<Caracter_Gengar>
+				<img width={"170px"} src={Gengar} />
+			</Caracter_Gengar>*/}
+			<FullWidthBox>
+				<H1_Styled className="custom-h1">Patrocinadores</H1_Styled>
+			</FullWidthBox>
 			<FooterStyled>
-				<FooterPart1></FooterPart1>
+				<FooterPart1>
+					<Icons>
+						<Icon_Button
+							href="https://www.instagram.com/emakersjr/"
+							target="_blank"
+							rel="noopener noreferrer">
+							<InstagramIcon />
+						</Icon_Button>
+						<Icon_Button
+							href="https://www.facebook.com/emakersjr"
+							target="_blank"
+							rel="noopener noreferrer">
+							<FacebookIcon />
+						</Icon_Button>
+						<Icon_Button
+							href="https://www.linkedin.com/company/emakersjr"
+							target="_blank"
+							rel="noopener noreferrer">
+							<LinkedInIcon />
+						</Icon_Button>
+						<Icon_Button
+							href="mailto:emakers.junior@ufla.br"
+							target="_blank"
+							rel="noopener noreferrer">
+							<EmailIcon />
+						</Icon_Button>
+					</Icons>
+				</FooterPart1>
+				<FooterPart2>
+					<Icon_Footer>
+						<img src={Logo_Branco} />
+					</Icon_Footer>
+					<P_Styled>Todos os direitos reservados - Emakers jr 2024</P_Styled>
+				</FooterPart2>
 			</FooterStyled>
 		</Screen5>
 	);
